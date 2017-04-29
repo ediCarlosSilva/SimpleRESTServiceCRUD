2e0bf5709b987c520eeae1d9b15b00e6e819cdd0
Edi Carlos note <edi_csilva@hotmail.com>
2017-04-29 14:26:37 -0300

    primeiro commit

---
+++ SimpleRESTServiceCRUD/BookService.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleRESTServiceCRUD
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookService.svc or BookService.svc.cs at the Solution Explorer and start debugging.
+    public class BookService : IBookService
+    {
+        static IBookRepository repository = new BookRepository();
+        public List<Book> GetBookList()
+        {
+            return repository.GetAllBooks();
+        }
+
+        public Book GetBookById(string id)
+        {
+            return repository.GetBookById(int.Parse(id));
+        }
+
+        public string AddBook(Book book, string id)
+        {
+            Book newBook = repository.AddNewBook(book);
+            return "id=" + newBook.BookId;
+        }
+
+        public string UpdateBook(Book book, string id)
+        {
+            bool updated = repository.UpdateABook(book);
+        }
+
+    }
+}
